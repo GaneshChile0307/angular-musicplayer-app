7c406c8164be7c3f62657e235072ba93cf14a462
Ganesh.Chile <ganesh.chile@mtm.org>
2024-03-29 14:55:49 +0100

    chore: initinal working code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MusicPlayerMainPageComponent } from './music-player-main-page/music-player-main-page.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MusicPlayerMainPageComponent
+    MusicPlayerMainPageComponent,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
---
+++ src/app/models/songs.ts
@@ -0,0 +1,7 @@
+export interface ISong {
+    id: number;
+    songArtist: string,
+    songTitle: string;
+    songUrl: string;
+    coverArt: string
+  }
